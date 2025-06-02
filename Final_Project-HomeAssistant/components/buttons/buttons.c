6a93f3fca0b26d4a41ab6c574528abc64ffd0a61
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2025-06-02 22:33:09 +0200

    added potentiometer activation setting

--- Final_Project-HomeAssistant/components/buttons/buttons.c
+++ Final_Project-HomeAssistant/components/buttons/buttons.c
@@ -26,12 +26,17 @@ static void IRAM_ATTR gpio_isr_handler(void* arg) {
 
 void buttons_init() {
     gpio_config_t gpioConfigIn = {
-        .pin_bit_mask = (1 << BUTTON_GPIO_LEFT) | (1 << BUTTON_GPIO_RIGHT),
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_ANYEDGE
     };
+    #if CONFIG_POTENTIOMETER_ACTIVE
+        gpioConfigIn.pin_bit_mask = (1 << BUTTON_GPIO);
+    #else
+        gpioConfigIn.pin_bit_mask = (1 << BUTTON_GPIO_LEFT) | (1 << BUTTON_GPIO_RIGHT);
+    #endif
+
     #if CONFIG_ENABLE_GPIO_PULLDOWN
         gpioConfigIn.pull_up_en = GPIO_PULLUP_DISABLE;
         gpioConfigIn.pull_down_en = GPIO_PULLDOWN_ENABLE;
@@ -41,15 +46,22 @@ void buttons_init() {
     gpio_install_isr_service(0);
     esp_err_t err;
 
-    err = gpio_isr_handler_add(BUTTON_GPIO_LEFT, gpio_isr_handler, (void*) BUTTON_GPIO_LEFT);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to add ISR handler for BUTTON_GPIO_LEFT: %s", esp_err_to_name(err));
-    }
+    #if CONFIG_POTENTIOMETER_ACTIVE
+        err = gpio_isr_handler_add(BUTTON_GPIO, gpio_isr_handler, (void*) BUTTON_GPIO);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to add ISR handler for BUTTON_GPIO: %s", esp_err_to_name(err));
+        }
+    #else
+        err = gpio_isr_handler_add(BUTTON_GPIO_LEFT, gpio_isr_handler, (void*) BUTTON_GPIO_LEFT);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to add ISR handler for BUTTON_GPIO_LEFT: %s", esp_err_to_name(err));
+        }
 
-    err = gpio_isr_handler_add(BUTTON_GPIO_RIGHT, gpio_isr_handler, (void*) BUTTON_GPIO_RIGHT);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to add ISR handler for BUTTON_GPIO_RIGHT: %s", esp_err_to_name(err));
-    }
+        err = gpio_isr_handler_add(BUTTON_GPIO_RIGHT, gpio_isr_handler, (void*) BUTTON_GPIO_RIGHT);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to add ISR handler for BUTTON_GPIO_RIGHT: %s", esp_err_to_name(err));
+        }
+    #endif
 
     create_buttonQueue();
 
@@ -57,7 +69,12 @@ void buttons_init() {
 }
 
 void buttons_cleanup() {
-    gpio_isr_handler_remove(BUTTON_GPIO_LEFT);
-    gpio_isr_handler_remove(BUTTON_GPIO_RIGHT);
+    #if CONFIG_POTENTIOMETER_ACTIVE
+        gpio_isr_handler_remove(BUTTON_GPIO);
+    #else
+        gpio_isr_handler_remove(BUTTON_GPIO_LEFT);
+        gpio_isr_handler_remove(BUTTON_GPIO_RIGHT);
+    #endif
+
     gpio_uninstall_isr_service();
 }
